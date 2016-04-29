ba62d37c5bb631bf384cd89f88ec554fccb5508b
Robert Mock <robertjamesmock@gmail.com>
2016-04-28 20:06:45 -0700

    autoformat

--- Game/Assets/views/ViewController.cs
+++ Game/Assets/views/ViewController.cs
@@ -3,29 +3,31 @@ using UnityEngine.SceneManagement;
 using sysObject = System.Object;
 using System.Collections;
 
-    public class ViewController : Singleton<ViewController>
+public class ViewController : Singleton<ViewController>
+{
+    public static SubView currentSubView;
+
+    private static string lastSceneLoaded;
+
+    
+
+    public static IEnumerator LoadViewInternal<T, U>(string sceneName, T arg, System.Action<U> callback)
     {
-        public static SubView currentSubView;
+        SceneManager.UnloadScene(lastSceneLoaded);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        lastSceneLoaded = sceneName;
 
-        private static string lastSceneLoaded;
+        yield return null;
 
-        public static IEnumerator LoadViewInternal<T,U>(string sceneName, T arg, System.Action<U> callback)
+        currentSubView = FindObjectOfType<SubView>(); ;
+        if (currentSubView != null)
         {
-            SceneManager.UnloadScene(lastSceneLoaded);
-            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-            lastSceneLoaded = sceneName;
-
-            yield return null;
-
-            currentSubView = FindObjectOfType<SubView>(); ;
-            if (currentSubView != null)
-            {
-                ((SubView<T,U>)currentSubView).BaseInit(arg, callback);
-            }
-            else
-            {
-                throw new UnityException("Scene name was valid, but no SubView was found");
-            }
+            ((SubView<T, U>)currentSubView).BaseInit(arg, callback);
+        }
+        else
+        {
+            throw new UnityException("Scene name was valid, but no SubView was found");
         }
     }
+}
 
